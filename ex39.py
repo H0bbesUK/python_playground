42ad2d5b5dc8409a8a4052ebc554a212d79c25ca
Darren Ryan <daz.ryan72@gmail.com>
2016-06-29 23:15:33 +0100

    Added treasure chest ASCII

--- ex39.py
+++ ex39.py
@@ -8,4 +8,22 @@ states = {
 }
 
 # create a basic set of states and some cities in them
-cities = {}
+cities = {
+    'CA': 'San Franciso',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
+}
+
+# add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+# print out some cities
+print '-' * 10
+print "NY State has: ", cities['NY']
+print "OR State has: ", cities['OR']
+print "CA State has: ", cities['CA']
+
+print '-' * 10
+print "Michigan's abbreviation is: ", states['Michigan']
+print "Florida's abbreviation is: ", states['FL'].keys()
