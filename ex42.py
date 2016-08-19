a55c461765e6829c185bea744f79a03fb32e7de0
Darren Ryan <daz.ryan72@gmail.com>
2016-08-19 15:17:53 +0100

    finished with ex42.py - classes

--- ex42.py
+++ ex42.py
@@ -14,7 +14,8 @@ class Cat(Animal):
     
     def __init__(self, name):
         ## ??
-        self.name
+        self.name = name
+        self.animal_type = "cat"
 
 ##??
 class Person(object):
@@ -31,3 +32,50 @@ class Employee(Person):
 
     def __init__(self, name, salary):
         ## ?? hmm what is this strange magic?
+        super(Employee, self).__init__(name)
+        ## ??
+        self.salary = salary
+
+## ??
+class Fish(object):
+    gills = True
+    legs = False
+
+## ??
+class Salmon(Fish):
+    pass
+
+## ??
+class Halibut(Fish):
+    pass
+
+
+## rover is-a Dog
+rover = Dog("Rover")
+
+## ??
+satan = Cat("Satan")
+
+mary = Person("Mary")
+
+## ??
+mary.pet = satan
+
+## ??
+frank = Employee("Frank", 120000)
+
+## ??
+frank.pet = rover
+
+## ??
+flipper = Fish()
+
+## ??
+crouse = Salmon()
+
+## ??
+harry = Halibut()
+
+print "Frank's salary is {0}, and has a pet called '{1}' \n".format(frank.salary, frank.pet.name)
+
+print "Mary's pet is a {0} and it's name is '{1}'".format(mary.pet.animal_type, mary.pet.name)
