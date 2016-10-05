846e5933e457f59beac4281de11ea652419b49b5
Darren Ryan <daz.ryan72@gmail.com>
2016-10-05 22:37:58 +0100

    more additions

--- classes_demo.py
+++ classes_demo.py
@@ -8,29 +8,30 @@ class Vehicle(object):
         self.colour = model
         wheels = 4
 
+
 class Car(Vehicle):
     """create the car class, child of Vehicle"""
     def __init__(self, colour, model):
         self.colour = colour
-        self.colour = model
+        self.model = model
 
 
 def details(veh_type, count):
     q1 = "What is the colour of the {0}? ".format(veh_type)
     q2 = "What is the model of the {0}? ".format(veh_type)
-    colour = raw_input(q1)
-    model = raw_input(q2)
+    colour = (raw_input(q1)).lower()
+    model = (raw_input(q2)).lower()
     print colour
     print model
     print count
-#    if veh_type == "Car":
-
-
+    transport = "transport" + str(count)
+    if veh_type == "car":
+        print transport
+        transport = Vehicle(colour, model)
 
 
 def select_veh(count):
     found = False
-    print "hello"
     print vehicle_options
     while found != True:
         vehicle = raw_input("What vehicle are you inputing? ")
@@ -44,8 +45,15 @@ def select_veh(count):
 
 if __name__ == "__main__":
 
-    i = 3
+    i = 1
     while i > 0:
         select_veh(i)
-        i -=1
-        print i
+        i -= 1
+        print "I = {0}".format(i)
+
+
+    for k in range(1,2):
+        print k
+        transport = "transport" + str(k)
+        print transport
+        print transport.model
